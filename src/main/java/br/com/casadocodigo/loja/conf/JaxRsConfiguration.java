78e57fddb5765f185235a5dd31586aab052afca5
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2017-11-15 01:21:30 -0200

    alterando comentário para commitar

--- src/main/java/br/com/casadocodigo/loja/conf/JaxRsConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JaxRsConfiguration.java
@@ -3,7 +3,7 @@ package br.com.casadocodigo.loja.conf;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/service") // indica o caminho que será atendido
+@ApplicationPath("/service") // indica o caminho que será atendidoh
 public class JaxRsConfiguration extends Application {
 
 }
